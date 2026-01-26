54d2e708ab94aa16ede76a5f884d652b3d345f96
tison <wander4096@gmail.com>
2026-01-26 15:14:47 +0800

    feat: port select_32 - column aliasing with expr_as

--- pqb/tests/select.rs
+++ pqb/tests/select.rs
@@ -445,3 +445,14 @@ fn select_31() {
         @r#"SELECT 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9"#
     );
 }
+
+#[test]
+fn select_32() {
+    assert_snapshot!(
+        Select::new()
+            .expr_as(Expr::column("character"), "C")
+            .from("character")
+            .to_sql(),
+        @r#"SELECT "character" AS "C" FROM "character""#
+    );
+}
